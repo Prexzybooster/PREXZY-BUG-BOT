0d28ac99f1d706587e21d189f3de9e697eaf0026
Precious Ayomide👑 <139078199+Prexzybooster@users.noreply.github.com>
2024-08-05 22:41:03 +0100

    PREXZYVILLA

---
+++ index.js
@@ -0,0 +1,34 @@
+//base by DGXeon (Xeon Bot Inc.)
+//re-upload? recode? copy code? give credit ya :)
+//YouTube: @DGXeon
+//Instagram: unicorn_xeon13
+//Telegram: t.me/xeonbotinc
+//GitHub: @DGXeon
+//WhatsApp: +916909137213
+//want more free bot scripts? subscribe to my youtube channel: https://youtube.com/@DGXeon
+
+const {
+   spawn
+} = require('child_process')
+const path = require('path')
+
+function start() {
+   let args = [path.join(__dirname, 'main.js'), ...process.argv.slice(2)]
+   console.log([process.argv[0], ...args].join('\n'))
+   let p = spawn(process.argv[0], args, {
+         stdio: ['inherit', 'inherit', 'inherit', 'ipc']
+      })
+      .on('message', data => {
+         if (data == 'reset') {
+            console.log('Restarting Bot...')
+            p.kill()
+            start()
+            delete p
+         }
+      })
+      .on('exit', code => {
+         console.error('Exited with code:', code)
+         if (code == '.' || code == 1 || code == 0) start()
+      })
+}
+start()
