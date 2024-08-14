846c3694b8ded765edb5cd7373cc0a8ef731571f
Precious Ayomide👑 <139078199+Prexzybooster@users.noreply.github.com>
2024-08-14 10:31:56 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,19 +2,19 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "2349159895444"
+global.ownernomer = "263717750825"
 global.ownername = "Precious Ayomide👑"
 global.ytname = "YT: Prexzyvilla"
 global.socialm = "GitHub: Prexzybooster"
 global.location = "Nigeria, Osun, Osogbo"
 
-global.ownernumber = '2349159895444'  //creator number
+global.ownernumber = '263717750825'  //creator number
 global.ownername = 'Precious Ayomide👑' //owner name
 global.botname = 'PREXZY ᵇᵘᵍ ᵇᵒᵗ' //name of the bot
 
 //sticker details
 global.packname = 'Sticker By'
-global.author = 'PREXZY👑\n\nContact: +2349159895444'
+global.author = 'PREXZY👑\n\nContact: +263717750825'
 
 //console view/theme
 global.themeemoji = '🪀'
