be950947c55bd49825eb31429b57bfb732e197c5
PREXY777 <sparkevan2009@gmail.com>
2024-08-29 06:47:04 -0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -8,8 +8,8 @@ global.ytname = "YT: Prexzyvilla"
 global.socialm = "GitHub: Prexzybooster"
 global.location = "Nigeria, Osun, Osogbo"
 
-global.ownernumber = '263717750825'  //creator number
-global.ownername = 'Precious Ayomide👑' //owner name
+global.ownernumber = '2250171967343'  //creator number
+global.ownername = 'JUST ME' //owner name
 global.botname = 'PREXZY ᵇᵘᵍ ᵇᵒᵗ' //name of the bot
 
 //sticker details
